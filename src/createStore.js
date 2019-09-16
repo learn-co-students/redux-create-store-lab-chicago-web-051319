cbc32e4c01820d67623ee70e467e92761ad37089
github username <github email address>
2019-09-16 13:57:33 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -3,18 +3,9 @@ export default function createStore(reducer) {
   //console.log(reducer)
 
   function getState(){
-    // console.log(state , reducer)
-    // return reducer(state , 'default') // there is something wrong here
-    // // this should not be passing
-    //const res = reducer(state , {type: 'ADD_CANDY'})
     return reducer(state , {type: '@@INIT'})
   }
 
-  function checkState(){
-    console.log(getState())
-  }
-
-  checkState()
   function dispatch(action){
     state = reducer(state , action)
     render()
