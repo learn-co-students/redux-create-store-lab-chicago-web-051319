f825fa83744d6af045d556e72ad415ee125e664a
github username <github email address>
2019-09-16 13:57:14 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,29 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+  //console.log(reducer)
+
+  function getState(){
+    // console.log(state , reducer)
+    // return reducer(state , 'default') // there is something wrong here
+    // // this should not be passing
+    //const res = reducer(state , {type: 'ADD_CANDY'})
+    return reducer(state , {type: '@@INIT'})
+  }
+
+  function checkState(){
+    console.log(getState())
+  }
+
+  checkState()
+  function dispatch(action){
+    state = reducer(state , action)
+    render()
+  }
+
+  return {getState , dispatch}
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
